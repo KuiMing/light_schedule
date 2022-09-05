22d3f1374be86c091b0fdf0c5e6ccd976355496a
KuiMing <benjamin0901@gmail.com>
2022-09-05 10:51:19 +0800

    2 minutes get hello

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,13 +1,14 @@
 import datetime
 import logging
-
+import requests
 import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-
+    res = requests.get("https://momagiclight.azurewebsites.net/hello")
+    print(res.status_code)
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
